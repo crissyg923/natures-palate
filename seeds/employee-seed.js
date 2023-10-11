6435db94480dad1ab0a4678b6e1364e28624ea0e
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-10-10 21:18:22 -0400

    added employeeRoutes file, made changes in employee.handlebar, changed the emp model and seeded it again

--- seeds/employee-seed.js
+++ seeds/employee-seed.js
@@ -3,9 +3,15 @@ const { Employee } = require('../models');
 const employeeData = [
   {
     name: 'Alex',
+    position: "chef",
+    address: "1 St, NY",
+    password: 'aaaa'
   },
   {
     name: 'Joel',
+    position: "Owner",
+    address: "2 St, MA",
+    password: 'bbbb'
   },
   
 ];
