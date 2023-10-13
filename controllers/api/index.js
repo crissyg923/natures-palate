6fe895adecd72df14066f8bde997d69f5df85d97
Crystal Green <crissybrown1992@icloud.com>
2023-10-12 22:32:17 -0400

    fixed jimp server.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,10 @@
 const router = require('express').Router();
 const orderRoutes = require('./orderRoutes');
-<<<<<<< HEAD
-// const projectRoutes = require('./projectRoutes');
-=======
 const dishRoutes = require('./dishRoutes');
 
 router.use('/orders', orderRoutes);
 router.use('/dishes', dishRoutes);
 
->>>>>>> b31bcf89e3842b59f8b923bfe3273d5d471ab7f6
 const employeeRoutes = require('./employeeRoutes');
 router.use('/orders', orderRoutes);
 
