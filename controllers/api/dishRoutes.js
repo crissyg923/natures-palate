9a0316270f83c203f629e343c212f7dc9a0885c2
Lima <linam.errico@gmail.com>
2023-10-14 18:03:30 -0400

    corrected menu delete and formating of menu screen

--- controllers/api/dishRoutes.js
+++ controllers/api/dishRoutes.js
@@ -1,48 +1,42 @@
 const router = require('express').Router();
 const { Dish } = require('../../models');
-
+const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     const dishData = await Dish.findAll();
-    
     const dishes = dishData.map((dish) => dish.get({ plain: true }));
-
     res.render('dishes',{ dishes } );
-   
   } catch (err) {
     console.log('Error:', err);
     res.status(500).json(err);
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     const newDish = await Dish.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-
     res.status(200).json(newDish);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
   try {
     const dishData = await Dish.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        //user_id: req.session.user_id,
       },
     });
-
     if (!dishData) {
       res.status(404).json({ message: 'No dish found with this id!' });
       return;
     }
-
     res.status(200).json(dishData);
   } catch (err) {
     res.status(500).json(err);
