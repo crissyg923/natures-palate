87aafb0c9db15255378622291123252c1c947586
Crystal Green <crissybrown1992@icloud.com>
2023-10-14 13:02:33 -0400

    added styling

--- public/js/signup.js
+++ public/js/signup.js
@@ -26,6 +26,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
+        
         document.location.replace('/login');
       } else {
         console.log('Error response:', response); 
